e5063a30a5fa7bfbd998f8a7796f2de0ef5c3281
Your Name~clear <mohammadsajadjafariazad@gmail.com>
2023-03-09 11:47:20 +0330

    first commit

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require("express")
+const fileUpload = require("express-fileupload")
 const upload = require("express-fileupload")
+const path = require("path")
 
-app.get('/upload', function(req, res) {
-    console.log(req.files.foo); // the uploaded file object
-  });
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use(fileUpload())
+
+
+app.post('express-fileuploaded',(req,res,next)=>{
+  const file = req.files.file
+  const filename = file.name
+  const size = file.data.length
+})
+
+
+
 
   
 
