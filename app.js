e79015b95eee332378fa02eacf7c23c01a67fe8a
Your Name~clear <mohammadsajadjafariazad@gmail.com>
2023-03-09 11:39:04 +0330

    start

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const upload = require("express-fileupload")
+
+app.get('/upload', function(req, res) {
+    console.log(req.files.foo); // the uploaded file object
+  });
+
+  
+
+
+
+
+
+
+
+
+
+
+app.listen("500")
